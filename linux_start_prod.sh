1ff27a72d34a925eceda2b6b781ac36e8b555e19
Ubuntu <ubuntu@ip-172-31-23-141.ec2.internal>
2025-01-05 16:03:04 +0000

    linux start

--- linux_start_prod.sh
+++ linux_start_prod.sh
@@ -1,3 +1,3 @@
-exec bash
-source .venv/bin/activate
-nohup python src/app.py >newsreader.log 2>&1 &
+pkill -9 -f "python src/app.py"
+nohup .venv/bin/python src/app.py >newsreader.log 2>&1 &
+echo ps -ef | grep python
